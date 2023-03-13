93b8c5575546d7912e40604a4916f12adb7ac562
collie <collie1158@qq.com>
2023-03-13 19:46:34 +0800

    add coding demo

--- interview/test.go
+++ interview/test.go
@@ -2,16 +2,28 @@ package main
 
 import "fmt"
 
-//	func main() {
-//		var m sync.Map
-//		m.Store("address", map[string]string{"province": "江苏", "city": "南京"})
-//		v, _ := m.Load("address")
-//		fmt.Printf("%+v\n", v)
-//	}
-
 func main() {
-	x := []string{"a", "b", "c"}
-	for v := range x {
-		fmt.Print(v)
+	str := "abcd"
+	getSubStr([]byte(str))
+}
+func getSubStr(str []byte) {
+	if len(str) == 0 {
+		return
 	}
+
+	if len(str) > 0 {
+		i := 0
+		l := 1
+		max := len(str)
+		for l <= max {
+			if i+l-1 < max {
+				fmt.Println(string(str[i : i+l]))
+				i++
+			} else {
+				i = 0
+				l++
+			}
+		}
+	}
+	return
 }
